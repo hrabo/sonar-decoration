f975f038083679b587deccff72217e998566145a
Gyorgy Hrabovszki <gyorgy.hrabovszki@gmail.com>
2021-03-15 01:41:45 +0100

    chore: Add hashCode

--- src/main/java/hrabovszki/weather/domain/Weather.java
+++ src/main/java/hrabovszki/weather/domain/Weather.java
@@ -33,4 +33,8 @@ public final class Weather {
                 Objects.equals(this.temperature, that.temperature);
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, temperature);
+    }
 }
